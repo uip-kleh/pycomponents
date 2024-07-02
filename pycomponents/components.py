c408b9a0306de44db48b41fbabc91a595a9ee084
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2024-07-02 12:26:13 +0900

    join all components

--- pycomponents/components.py
+++ pycomponents/components.py
@@ -1,9 +1,11 @@
 import os
 import sys
 sys.path.append(os.pardir)
+from pycomponents.system import System
+from pycomponents.io import IO
 from pycomponents.draw import Draw 
 
-class Componetns(Draw):
+class Componetns(System, IO, Draw):
     def __init__(self) -> None:
         super().__init__()
 
